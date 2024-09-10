f263820d1a63772e6c85aca14f692720aeaa7ec7
Andrew <andrew.gary91@gmail.com>
2024-09-10 15:11:27 -0500

    fixing other db problem

--- dbUtils/utils.js
+++ dbUtils/utils.js
@@ -29,7 +29,7 @@ const getConnection = async (maxRetries = 3) => {
   while (attempts < maxRetries) {
     try {
       const conn = await mariadb.createConnection({
-        host: host,
+        host: 'localhost',
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
